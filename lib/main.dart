f7419681a191cc457063099ff4934a773e03f021
mj <onyangomaureen95@gmail.com>
2019-10-15 14:56:03 +0300

    import image picker and place image selection

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:mlkit/mlkit.dart';
+import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 
-void main(){
+void main() {
   runApp(MLflutter());
 }
 
-class MLflutter extends StatefulWidget{
-  createState(){
+class MLflutter extends StatefulWidget {
+  createState() {
     return MLflutterState();
   }
 }
-class MLflutterState extends State<MLflutter>{
-File _file;
-List<VisionFace> _face = <VisionFace>[];
 
-VisionFaceDetectorOptions options = VisionFaceDetectorOptions(
-  modeType: VisionFaceDetectorMode.Accurate,
-  landmarkType: VisionFaceDetectorLandmark.All,
-  classificationType: VisionFaceDetectorClassification.All,
-  minFaceSize: 0.15,
-  isTrackingEnabled: true,
-  
-);
+class MLflutterState extends State<MLflutter> {
+  File _file;
+  List<VisionFace> _face = <VisionFace>[];
 
-FirebaseVisionTextDetector detector = FirebaseVisionTextDetector.instance;
+  VisionFaceDetectorOptions options = VisionFaceDetectorOptions(
+    modeType: VisionFaceDetectorMode.Accurate,
+    landmarkType: VisionFaceDetectorLandmark.All,
+    classificationType: VisionFaceDetectorClassification.All,
+    minFaceSize: 0.15,
+    isTrackingEnabled: true,
+  );
 
-  Widget build(context){
+  FirebaseVisionTextDetector detector = FirebaseVisionTextDetector.instance;
+
+  @override
+  initState() {
+    super.initState();
+  }
+
+  Widget build(context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.red,
-        accentColor: Colors.tealAccent
-      ),
+      theme:
+          ThemeData(primaryColor: Colors.red, accentColor: Colors.tealAccent),
       title: 'Flutter + ML',
       home: Scaffold(
         appBar: AppBar(
           title: Text('Flutter + ML'),
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            var file = await ImagePicker.pickImage(
+              source: ImageSource.gallery );
+            setState(() {
+              _file= file;
+            });
+          },
+        ),
+        body: showBody(_file),
       ),
-    )
-    ;
+    );
   }
 }
