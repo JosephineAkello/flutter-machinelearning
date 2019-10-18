9439dbc17a082289990ff3aa83bb7e30ab79fa01
mj <onyangomaureen95@gmail.com>
2019-10-18 16:52:48 +0300

    format doc

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:mlkit/mlkit.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+import 'package:mlkit/mlkit.dart';
+import 'dart:async';
 
-void main() {
-  runApp(MLflutter());
+void main() => runApp(new  MLflutter());
+
+class  MLflutter extends StatefulWidget {
+  @override
+   MLflutterState createState() => new MLflutterState();
 }
 
-class MLflutter extends StatefulWidget {
-  createState() {
-    return MLflutterState();
-  }
-}
-
-class MLflutterState extends State<MLflutter> {
+class  MLflutterState extends State< MLflutter> {
   File _file;
+
   List<VisionFace> _face = <VisionFace>[];
 
-  VisionFaceDetectorOptions options = VisionFaceDetectorOptions(
-    modeType: VisionFaceDetectorMode.Accurate,
-    landmarkType: VisionFaceDetectorLandmark.All,
-    classificationType: VisionFaceDetectorClassification.All,
-    minFaceSize: 0.15,
-    isTrackingEnabled: true,
-  );
+  VisionFaceDetectorOptions options = new VisionFaceDetectorOptions(
+      modeType: VisionFaceDetectorMode.Accurate,
+      landmarkType: VisionFaceDetectorLandmark.All,
+      classificationType: VisionFaceDetectorClassification.All,
+      minFaceSize: 0.15,
+      isTrackingEnabled: true);
 
-  FirebaseVisionTextDetector detector = FirebaseVisionTextDetector.instance;
+  FirebaseVisionFaceDetector detector = FirebaseVisionFaceDetector.instance;
 
   @override
   initState() {
     super.initState();
   }
 
-  Widget build(context) {
-    return MaterialApp(
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme:
-          ThemeData(primaryColor: Colors.red, accentColor: Colors.tealAccent),
-      title: 'Flutter + ML',
-      home: Scaffold(
+      theme: ThemeData(
+        primaryColor: Colors.red,
+        accentColor: Colors.pink[50],),
+      home:  Scaffold(
         appBar: AppBar(
-          title: Text('Flutter + ML'),
+          title: Text('Flutter Face Detection'),
         ),
         body: showBody(_file),
-        floatingActionButton: FloatingActionButton(
+        floatingActionButton: new FloatingActionButton(
           onPressed: () async {
             var file = await ImagePicker.pickImage(source: ImageSource.gallery);
             setState(() {
               _file = file;
             });
+
             var face = await detector.detectFromBinary(
                 _file?.readAsBytesSync(), options);
             setState(() {
@@ -59,13 +59,167 @@ class MLflutterState extends State<MLflutter> {
               }
             });
           },
-          child: new Icon(Icons.tag_faces),
+          child: new Icon(Icons.file_upload),
         ),
       ),
     );
   }
 
   Widget showBody(File file) {
-    return Container();
+    return new Container(
+      child: new Stack(
+        children: <Widget>[
+          _buildImage(),
+          _showDetails(_face),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildImage() {
+    return new SizedBox(
+      height: 500.0,
+      child: new Center(
+        child: _file == null
+            ? new Text('Choose an image from the FAB Button')
+            : new FutureBuilder<Size>(
+                future: _getImageSize(Image.file(_file, fit: BoxFit.fitWidth)),
+                builder: (BuildContext context, AsyncSnapshot<Size> snapshot) {
+                  if (snapshot.hasData) {
+                    return Container(
+                        foregroundDecoration:
+                            TextDetectDecoration(_face, snapshot.data),
+                        child: Image.file(_file, fit: BoxFit.fitWidth));
+                  } else {
+                    return new Text('Loading please wait...');
+                  }
+                },
+              ),
+      ),
+    );
+  }
+}
+
+class TextDetectDecoration extends Decoration {
+  final Size _originalImageSize;
+  final List<VisionFace> _texts;
+  TextDetectDecoration(List<VisionFace> texts, Size originalImageSize)
+      : _texts = texts,
+        _originalImageSize = originalImageSize;
+
+  @override
+  BoxPainter createBoxPainter([VoidCallback onChanged]) {
+    return new _TextDetectPainter(_texts, _originalImageSize);
+  }
+}
+
+Future _getImageSize(Image image) {
+  Completer<Size> completer = new Completer<Size>();
+  image.image.resolve(new ImageConfiguration())
+  .addListener(ImageStreamListener(
+      (ImageInfo info, bool _) => completer.complete(
+          Size(info.image.width.toDouble(), info.image.height.toDouble()))));
+  return completer.future;
+}
+
+Widget _showDetails(List<VisionFace> faceList) {
+  if (faceList == null || faceList.length == 0) {
+    return new Text('', textAlign: TextAlign.center);
+  }
+  return new Container(
+    color: Colors.white12,
+    child: new ListView.builder(
+      padding: const EdgeInsets.all(10.0),
+      itemCount: faceList.length,
+      itemBuilder: (context, i) {
+        checkData(faceList);
+        return _buildRow(
+            faceList[0].hasLeftEyeOpenProbability,
+            faceList[0].headEulerAngleY,//indicates rotation of face towards vertical axis of the image
+            faceList[0].headEulerAngleZ,
+            faceList[0].leftEyeOpenProbability,
+            faceList[0].rightEyeOpenProbability,
+            faceList[0].smilingProbability,
+            faceList[0].trackingID);
+      },
+    ),
+  );
+}
+
+//Checks and prints various variables from firebase
+void checkData(List<VisionFace> faceList) {
+  final double uncomputedProb = -1.0;
+  final int uncompProb = -1;
+
+  for (int i = 0; i < faceList.length; i++) {
+    Rect bounds = faceList[i].rect;
+    print('Rectangle : $bounds');
+
+    VisionFaceLandmark landmark =
+        faceList[i].getLandmark(FaceLandmarkType.LeftEar);
+
+    if (landmark != null) {
+      VisionPoint leftEarPos = landmark.position;
+      print('Left Ear Pos : $leftEarPos');
+    }
+
+    if (faceList[i].smilingProbability != uncomputedProb) {
+      double smileProb = faceList[i].smilingProbability;
+      print('Smile Prob : $smileProb');
+    }
+
+    if (faceList[i].rightEyeOpenProbability != uncomputedProb) {
+      double rightEyeOpenProb = faceList[i].rightEyeOpenProbability;
+      print('RightEye Open Prob : $rightEyeOpenProb');
+    }
+
+    if (faceList[i].trackingID != uncompProb) {
+      int tID = faceList[i].trackingID;
+      print('Tracking ID : $tID');
+    }
+  }
+}
+
+Widget _buildRow(
+    bool leftEyeProb,
+    double headEulerY,
+    double headEulerZ,
+    double leftEyeOpenProbability,
+    double rightEyeOpenProbability,
+    double smileProb,
+    int tID) {
+  return ListTile(
+    title: new Text(
+      "\nLeftEyeProb: $leftEyeProb \nHeadEulerY : $headEulerY \nHeadEulerZ : $headEulerZ \nLeftEyeOpenProbability : $leftEyeOpenProbability \nRightEyeOpenProbability : $rightEyeOpenProbability \nSmileProb : $smileProb \nFaceTrackingEnabled : $tID",
+    ),
+    dense: true,
+  );
+}
+
+class _TextDetectPainter extends BoxPainter {
+  final List<VisionFace> _faceLabels;
+  final Size _originalImageSize;
+  _TextDetectPainter(faceLabels, originalImageSize)
+      : _faceLabels = faceLabels,
+        _originalImageSize = originalImageSize;
+
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    final paint = new Paint()
+      ..strokeWidth = 2.0
+      ..color = Colors.red
+      ..style = PaintingStyle.stroke;
+
+    final _heightRatio = _originalImageSize.height / configuration.size.height;
+    final _widthRatio = _originalImageSize.width / configuration.size.width;
+    for (var faceLabel in _faceLabels) {
+      final _rect = Rect.fromLTRB(
+          offset.dx + faceLabel.rect.left / _widthRatio,
+          offset.dy + faceLabel.rect.top / _heightRatio,
+          offset.dx + faceLabel.rect.right / _widthRatio,
+          offset.dy + faceLabel.rect.bottom / _heightRatio);
+
+      canvas.drawRect(_rect, paint);
+    }
   }
 }
